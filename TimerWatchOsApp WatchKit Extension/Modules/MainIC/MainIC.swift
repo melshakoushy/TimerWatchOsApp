585b6a3e49c4a6f5b398bd0a1a35110f05e6ceab
Shakoushy <melshakoushy@hotmail.com>
2021-10-24 14:21:11 +0200

    MainIC Done and the timer is working

--- TimerWatchOsApp WatchKit Extension/Modules/MainIC/MainIC.swift
+++ TimerWatchOsApp WatchKit Extension/Modules/MainIC/MainIC.swift
@@ -11,16 +11,67 @@ import Foundation
 
 class MainIC: WKInterfaceController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var timer: WKInterfaceTimer!
+    @IBOutlet weak var startBtn: WKInterfaceButton!
+    @IBOutlet weak var totalCountTimer: WKInterfaceTimer!
+    
+    //MARK: - Variables
+    var isTimerStarted = false
+    var startTime = Date()
+    var elapsedTime: TimeInterval = 0.0
+    var totalStarted = false
+    
+    // MARK: - View Life Cycle
     override func awake(withContext context: Any?) {
-        // Configure interface objects here.
+        super.awake(withContext: context)
     }
     
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        timer.setDate(Date())
     }
     
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
     }
-
+    
+    // MARK: - Reset Function
+    func setTimer(timer: WKInterfaceTimer, interval: TimeInterval) {
+        if totalStarted && timer == totalCountTimer {
+            return
+        }
+        timer.setDate(Date(timeIntervalSinceNow: interval))
+        timer.start()
+    }
+    
+    // MARK: - Buttons Actions
+    @IBAction func startBtnPressed() {
+        isTimerStarted = !isTimerStarted
+        if isTimerStarted {
+            startTime = Date()
+            startBtn.setTitle("STOP")
+            setTimer(timer: timer, interval: elapsedTime)
+            setTimer(timer: totalCountTimer, interval: elapsedTime)
+            totalStarted = true
+        } else {
+            let stoppedTime = Date()
+            elapsedTime -= stoppedTime.timeIntervalSince(startTime)
+            startBtn.setTitle("START")
+            timer.stop()
+        }
+    }
+    
+    @IBAction func resetBtnPressed() {
+        isTimerStarted = false
+        startTime = Date()
+        elapsedTime = 0.0
+        totalStarted = false
+        startBtn.setTitle("START")
+        timer.setDate(startTime)
+        timer.stop()
+        totalCountTimer.setDate(startTime)
+        totalCountTimer.stop()
+    }
+    
 }
